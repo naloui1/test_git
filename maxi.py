0d5326e652deaa50b89ac577e23d23773314ff52
Nadia <nadia.aloui@le-campus-numerique.fr>
2023-10-17 12:39:55 +0200

    Fisrt release of maximum

---
+++ maxi.py
@@ -0,0 +1,29 @@
+
+def maximum(a,b):
+    if a <b:
+        max=b
+    else:
+        max=a
+    return max
+
+print(maximum(4,1))
+
+
+def maximum3(a,b,c):
+    max=maximum(a,b)
+    max=maximum(max,c)
+    return max
+
+
+print(maximum3(4,12,1))
+
+def maximum3_input():
+    a=[]
+    for i in range(0,3):
+        b=int(input('Entier?'))  
+        a.append(b) 
+    max=maximum3(a[0],a[1],a[2])
+    return max
+
+
+print(maximum3_input())
